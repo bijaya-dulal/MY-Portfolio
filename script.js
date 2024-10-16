53065b747a98c110018a12c4ff9442f3b3aec40f
bijaya-dulal <dulalbijaya0@gmail.com>
2024-10-17 00:39:35 +0545

    logo added

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 // typed js
 
 const typed = new Typed('.multiple-text', { 
-    strings: ['Engineering Student', 'Python Developer', 'Backend Developer'],
+    strings: ['Engineering Student', 'Python Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 500,
